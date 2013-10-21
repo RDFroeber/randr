a6cd39b9d42f69214ca47e7cd912f9d1fda1484e
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-21 19:57:01 -0400

    Controllers added along with session helper

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+   def new
+       render :new
+   end
+
+   def create
+      # Authenticate that user and password combo is legit
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+      # Save the session
+         session[:user_id] = user.id
+         redirect_to user_path(user)
+      else
+         redirect_to new_session_path
+      end
+   end
+
+   def destroy
+     session[:user_id] = nil
+     redirect_to artists_path
+   end
+
+end
