dd6b27bb2686f15961ff50f174b57733722e1b55
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-21 18:47:37 -0400

    Authors table generated and raked

--- db/migrate/20131021224422_add_authors.rb
+++ db/migrate/20131021224422_add_authors.rb
@@ -1,4 +1,9 @@
 class AddAuthors < ActiveRecord::Migration
   def change
+   create_table :authors do |t|
+      t.string :name, null: false
+      t.boolean :alive, null: false
+      t.timestamps
+    end
   end
 end
