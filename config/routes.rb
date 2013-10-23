0ffee8774a8170bcfda0fd1b661191f4fd37bcf5
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-23 02:34:18 -0400

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Randr::Application.routes.draw do
   resources :users, except: [:index] 
   # remove unused paths
-  resources :authors, only: [:index, :new, :create, :show] do
+  resources :authors, only: [:new, :create, :show] do
       collection do
          get "search"
       end
    end
 
   # resources :books, only: [:show]
-  resources :favorites, only: [:create, :edit, :destroy]
+  # resources :favorites, only: [:create, :edit, :destroy]
 
   resource :session, only: [:new, :create, :destroy]
   
