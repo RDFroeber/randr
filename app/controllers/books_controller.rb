8ddade8b2fc0c11c56ddc6211978031f96b976d4
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-24 15:48:22 -0400

    Logout functionality added and Nav updated

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -39,7 +39,7 @@ class BooksController < ApplicationController
             @no_info = "Sorry, there is not enough information for that title."
          elsif book_obj['ItemAttributes']['Title'] == params[:title] && !book_obj['ItemAttributes']['ISBN'].nil?
             @new_book = book_obj
-         elsif !book_obj['ItemAttributes']['ISBN'].nil?
+         elsif !book_obj['ItemAttributes']['ISBN'].nil? && !book_obj['ItemAttributes']['ISBN'].nil? && !book_obj['ItemAttributes']['PublicationDate'].nil?
             @choose_book.push(book_obj)
          end
       end
@@ -49,8 +49,9 @@ class BooksController < ApplicationController
 
    def create
       #if book already exists just use that data entry
-      author_id = Author.find_by(name: params[:author]).id
-      book = Book.find_or_initialize_by(title: params[:title], author_id: author_id)
+      author = Author.find_or_initialize_by(name: params[:author])
+      author.save
+      @book = Book.find_or_initialize_by(title: params[:title], author_id: author.id)
 
       req = Vacuum.new
       path = File.join(Rails.root, 'config', 'amazon.yml')
@@ -70,22 +71,28 @@ class BooksController < ApplicationController
       response = Response.new(res).to_h
       book_res = response['ItemSearchResponse']['Items']['Item'][0]
 
-      book.isbn =  book_res['ItemAttributes']['ISBN']
-      book.published_date = book_res['ItemAttributes']['PublicationDate']
+      @book.isbn =  book_res['ItemAttributes']['ISBN']
+      if !book_res['ItemAttributes']['PublicationDate'].nil?
+         @book.published_date = book_res['ItemAttributes']['PublicationDate']
+      elsif !book_res['ItemAttributes']['ReleaseDate'].nil?
+         @book.published_date = book_res['ItemAttributes']['ReleaseDate']
+      else
+         @book.published_date = "2000-01-01"
+      end
       if !book_res["MediumImage"].nil?
-            book.img_url_sm = book_res["MediumImage"]["URL"]
-         end
-         if !book_res["LargeImage"].nil?
-            book.img_url_lg = book_res["LargeImage"]["URL"]
-         end
-      book.save
+         @book.img_url_sm = book_res["MediumImage"]["URL"]
+      end
+      if !book_res["LargeImage"].nil?
+         @book.img_url_lg = book_res["LargeImage"]["URL"]
+      end
+      @book.save
       
       # binding.pry
       # Add book to user library
       lib = Library.find_or_initialize_by(user_id: @current_user.id, book_id: book.id)
       lib.save
 
-      redirect_to library_users_path(@current_user)
+      redirect_to library_user_path(@current_user)
    end
 
    def show
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
    def destroy
      session[:user_id] = nil
-     redirect_to artists_path
+     redirect_to new_user_path
    end
 
 end
