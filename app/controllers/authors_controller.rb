81ec23c0b5127f605be35d033117312f166a24c3
Raleigh D Froeber <RDFroeber@gmail.com>
2013-10-25 14:28:25 -0400

    Added Immediate Future Release Data

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -61,6 +61,10 @@ class AuthorsController < ApplicationController
       fav = Favorite.find_or_initialize_by(user_id: @current_user.id, author_id: @author.id)
       fav.save
 
+      lookup = Search.new
+      search = lookup.new_favorites
+      lookup.save_favorites(search)
+   
       redirect_to user_path(@current_user)
    end
 
