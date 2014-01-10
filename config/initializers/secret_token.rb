3b1238a43ea58a3446f6877e0fd04e54888f51d8
Raleigh D Froeber <RDFroeber@gmail.com>
2014-01-09 19:32:42 -0500

    Gitignore updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Randr::Application.config.secret_key_base = '0b03ce8b7b81aed1138edf32217a35705369fdf8ce1b4956a0549e1d7392851c145b956af5d9348a2b08174884b9f06e91847c084f14b08430aee2aca929f264'
